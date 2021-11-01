b419f8fb284ab1d8036bb13c438d098b799263fa
autopp <autopp.inc@gmail.com>
2021-11-02 00:16:35 +0900

    test: add test case for Finder

--- pkg/finder/finder_test.go
+++ pkg/finder/finder_test.go
@@ -11,9 +11,12 @@ var _ = Describe("Finder", func() {
 		func(excludedList []string, expected []string) {
 			f, _ := New(WithExcludedList(excludedList))
 			actual, err := f.FindGoFiles("testdata")
-			Expect(err).ShouldNot(HaveOccurred())
+			Expect(err).NotTo(HaveOccurred())
 			Expect(actual).To(Equal(expected))
 		},
 		Entry("with no exclude", []string{}, []string{"testdata/a/foo.go", "testdata/b/c/bar.go", "testdata/main.go"}),
+		Entry("with no exclude", []string{"testdata/main.go"}, []string{"testdata/a/foo.go", "testdata/b/c/bar.go"}),
+		Entry("with no exclude", []string{"testdata/a"}, []string{"testdata/b/c/bar.go", "testdata/main.go"}),
+		Entry("with no exclude", []string{"testdata/*/c"}, []string{"testdata/a/foo.go", "testdata/main.go"}),
 	)
 })
